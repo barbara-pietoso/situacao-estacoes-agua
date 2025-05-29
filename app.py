bf45e5531a6b00f475f3249a6310f63579ea9a99
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2025-05-29 10:38:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -63,18 +63,18 @@ if st.button("Consultar"):
               "DataInicio": data_inicio.strftime("%d/%m/%Y"),
               "DataFim": data_fim.strftime("%d/%m/%Y")
     }
-        try:
-            response = requests.get(url, params=params, timeout=10)
-            if response.status_code == 200 and "<DataHora>" in response.text:
-                # Verifica se há pelo menos um dado numérico válido
-                if any(tag in response.text for tag in ["<Valor>", "<Nivel>", "<Vazao>", "<Chuva>"]):
-                    return "ativa"
-                else:
-                    return "inativa"
-            else:
-                return "inativa"
-        except:
-            return "erro"
+          try:
+              response = requests.get(url, params=params, timeout=10)
+              if response.status_code == 200 and "<DataHora>" in response.text:
+                  # Verifica se há pelo menos um dado numérico válido
+                  if any(tag in response.text for tag in ["<Valor>", "<Nivel>", "<Vazao>", "<Chuva>"]):
+                      return "ativa"
+                  else:
+                      return "inativa"
+              else:
+                  return "inativa"
+          except:
+              return "erro"
 
 
         # Consulta os dados de cada estação
